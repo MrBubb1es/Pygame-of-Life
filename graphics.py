e73f1a435f11ebb7951627c21d12470eb73ff989
MrBubb1es <lance04preston@gmail.com>
2021-06-25 03:02:13 -0500

    GAMER MODE

--- graphics.py
+++ graphics.py
@@ -1,5 +1,5 @@
 from config import *
-import pygame
+import pygame, math
 
 # Size as a percentage of the display size
 toolbar_size = .07
@@ -19,6 +19,51 @@ def drawGame(board, game_surface):
     board_surface = pygame.transform.scale(board_surface, game_surface.get_size())
     game_surface.blit(board_surface, (0,0))
 
+# Reusing this from my sprite editor app
+def hueToRGB(hue):
+    # 0 <= hue < 60: R = 255, B = 0, Solve for G
+    # G starts at 0
+    if (0 <= hue) and (hue < 60):
+        R = 255
+        B = 0
+        G = round((1 - (60 - hue)/60) * 255)
+    # 60 <= hue < 120: G = 255, B = 0, Solve for R
+    # R starts at 255
+    elif (60 <= hue) and (hue < 120):
+        R = round((60 - (hue - 60))/60 * 255)
+        B = 0
+        G = 255
+    # 120 <= hue < 180: G = 255, R = 0, Solve for B
+    # B starts at 0
+    elif (120 <= hue) and (hue < 180):
+        R = 0
+        B = round((1 - (60 - (hue - 120))/60) * 255)
+        G = 255
+    # 180 <= hue < 240: B = 255, R = 0, Solve for G
+    # G starts at 255
+    elif (180 <= hue) and (hue < 240):
+        R = 0
+        B = 255
+        G = round((60 - (hue - 180))/60 * 255)
+    # 240 <= hue < 300: B = 255, G = 0, Solve for R
+    # R starts at 0
+    elif (240 <= hue) and (hue < 300):
+        R = round((1 - (60 - (hue - 240))/60) * 255)
+        B = 255
+        G = 0
+    # 300 <= hue < 360: R = 255, G = 0, Solve for B
+    # B starts at 255
+    elif (300 <= hue) and (hue < 360):
+        R = 255
+        B = round((60 - (hue - 300))/60 * 255)
+        G = 0
+    else:
+        print("Hue {} not within acceptable bounds of 0 to 360".format(hue))
+        R = 255
+        G = 255
+        B = 255
+
+    return (R,G,B)
 
 def drawScreen(board, screen):
     screen_width, screen_height = pygame.display.get_surface().get_size()
@@ -36,23 +81,15 @@ def drawScreen(board, screen):
     for k, y in enumerate(board):
         for l, x in enumerate(y):
             if (board[k][l] == 1):
-                board_surface.set_at((k, l), (255,255,255))
+                color = hueToRGB(k / BOARD_SIZE[0] * 360)
             else:
-                board_surface.set_at((k, l), (0,0,0))
+                color = (0,0,0)
+            board_surface.set_at((k,l), color)
+
 
     # Scale board_surface to fit the window
-    print(board_surface.get_size())
     board_surface = pygame.transform.scale(board_surface, (screen_width, screen_height))
-    print(board_surface.get_size())
 
     #screen.blit(toolbar_surface, (0,0))
     screen.blit(board_surface, (0,0))
     pygame.display.update()
-
-
-def printGame(board):
-    for y in board:
-        for x in y:
-            print("#" if x == 1 else ".", end=" ")
-        print()
-    print()
--- main.py
+++ main.py
@@ -1,66 +1,55 @@
 from config import *
 from graphics import *
 from math import floor
-import pygame
+import pygame, random
+from pygame.locals import *
+from numba import jit, cuda
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
+
+screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(screen_size, RESIZABLE)
 clock = pygame.time.Clock()
 
 paused = True
 
 def newBoard():
-    return [[0 for x in range(BOARD_SIZE[0])] for y in range(BOARD_SIZE[1])]
+    return [[random.randint(0,1) for x in range(BOARD_SIZE[0])] for y in range(BOARD_SIZE[1])]
 
 def main():
     board = newBoard()
-
-    board[0][1] = 1
-    board[1][2] = 1
-    board[2][0] = 1
-    board[2][1] = 1
-    board[2][2] = 1
 
     running = True
     while running:
         drawScreen(board, screen)
         board = update(board)
-
-        drawScreen(board, screen)
         clock.tick(HZ)
 
-        running = not checkBlank()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
 
-# Check if the board is blank
-def checkBlank(board):
-    blank = True
+            elif event.type == VIDEORESIZE:
+                screen_size = (event.w, event.h)
 
-    for y in board:
-        for x in y:
-            if x == 1:
-                blank = False
 
-    return blank
+    pygame.quit()
 
 def countNeighbors(board, x, y):
     aliveNeighbors = 0
     for i in range(x-1, x+2):
         for j in range(y-1, y+2):
             if i < 0 or i >= BOARD_SIZE[0]:
-                print("i < 0 or i >= BOARD_SIZE[0] : i,j = ({}, {}), x,y = ({}, {}), BS[0],BS[1] = ({}, {})".format(i,j,x,y,BOARD_SIZE[0],BOARD_SIZE[1]))
                 continue
             elif j < 0 or j >= BOARD_SIZE[1]:
-                print("j < 0 or j >= BOARD_SIZE[1] : i,j = ({}, {}), x,y = ({}, {}), BS[0],BS[1] = ({}, {})".format(i,j,x,y,BOARD_SIZE[0],BOARD_SIZE[1]))
                 continue
             elif i == x and y == j:
-                print("i == x and j == y : i,j = ({}, {}), x,y = ({}, {}), BS[0],BS[1] = ({}, {})".format(i,j,x,y,BOARD_SIZE[0],BOARD_SIZE[1]))
                 continue
             else:
-                print("Neighbor is {} : i,j = ({}, {}), x,y = ({}, {}), BS[0],BS[1] = ({}, {}) board = {}".format(("alive" if board[i][j] == 1 else "dead"),i,j,x,y,BOARD_SIZE[0],BOARD_SIZE[1], board))
                 aliveNeighbors += board[j][i]
-    print("{} live neighbor(s)".format(aliveNeighbors))
     return aliveNeighbors
 
+@jit(target="cuda")
 def update(board):
     new = newBoard()
     for j in range(BOARD_SIZE[1]):
@@ -68,14 +57,11 @@ def update(board):
             neighbors = countNeighbors(board, i, j)
             if neighbors < 2 or neighbors >= 4: # under- or over-population
                 new[j][i] = 0 # update the back board
-                pass
             elif neighbors == 3:
                 new[j][i] = 1
-                pass
             else:
                 new[j][i] = board[j][i]
-                pass
 
-    return new # update board to the new (back) board
+    return new
 
 main()
